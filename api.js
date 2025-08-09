cd32cff988dd03a9734e89307201fe09462de7d6
Alivia-Wynn <128658839+Alivia-Wynn@users.noreply.github.com>
2025-08-08 22:22:55 -0500

    Add files via upload
    redesigned key pages, added family tree api.

--- api.js
+++ api.js
@@ -6,3 +6,18 @@ async function getData(){
 }
 
 getData()
+
+var chart = new OrgChart(document.getElementById("tree"), {
+    template: "isla",
+    enableSearch: false,
+    mouseScrool: OrgChart.action.none,
+    nodeBinding: {
+        field_0: "name",
+        field_1: "title"
+    },
+    nodes: [
+        { id: 1, name: "Amber McKenzie", title: "CEO" },
+        { id: 2, pid: 1, name: "Ava Field", title: "IT Manager" },
+        { id: 3, pid: 1, name: "Rhys Harper" }
+    ]
+});
